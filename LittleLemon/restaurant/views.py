92e133480fd87c2d80e2179313d434991105c864
Filip Kosowski <Filip.Mariusz.Kosowski@cern.ch>
2023-04-04 10:40:51 +0200

    Adding serializer, and view classes
    comment: new routing was added as well

--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
+from rest_framework import generics, viewsets, permissions
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated] 
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
